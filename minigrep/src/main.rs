acd785cf620a35a9def01cef1d68b81fc5b66cec
jacket <1762982273@qq.com>
2021-12-20 23:16:07 +0800

    feat(standard error): write error to standar error instead of output

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,13 +5,14 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
-    let config = Config::new(&args).unwrap_or_else(|_err| {
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(_e) = minigrep::run(config) {
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
